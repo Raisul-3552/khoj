09338cb5756d4268f1c06089e355c7aff6bc04bd
unknown <ashraful.cse.20230104030@aust.edu>
2025-07-29 16:15:04 +0600

    upadate in navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -21,6 +21,9 @@ const Navbar = () => {
             <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">Register</Link>
+            </li>
           </ul>
         </div>
       </div>
