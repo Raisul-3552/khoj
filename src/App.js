5a238e20bb8b96bf1a43710c3179d4456a23367f
unknown <ashraful.cse.20230104030@aust.edu>
2025-08-27 00:54:42 +0600

    update in app.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,29 @@
   import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import Registration from './component/Registration';
-  import 'bootstrap/dist/css/bootstrap.min.css';
-  import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-  
-  import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-  } from "react-router-dom";
-  
-  import Login from './component/Login'; 
-  import Navbar from './component/Navbar';
-  import Home from './component/Home';
-  import About from './component/About';
-<<<<<<< HEAD
-  
-=======
-  import Registration from './component/Registration'; 
-  import Report from './component/Report'; 
->>>>>>> f45736b30cc3925be99077c2c8b168849d8bd398
+import Login from './component/Login'; 
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import About from './component/About';
 
-  function App() {
-    return (
-      <Router>
-        <Navbar />
-        <div className="container mt-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Registration />} />
-            <Route path="/register" element={<Registration />} />
-            <Route path="/report" element={<Report />} />
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
-          </Routes>
-        </div>
-      </Router>
-    );
-  }
-
-  export default App;
+export default App;
